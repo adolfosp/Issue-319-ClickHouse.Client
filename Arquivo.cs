769c61989c4eecbd8599f6084f4ee812382f8167
Adolfo Poiatti <adolfo.poiatti@inovafarma.com.br>
2023-06-26 08:59:35 -0300

    adds example

---
+++ Arquivo.cs
@@ -0,0 +1,57 @@
+﻿namespace IssueClickHouseClient;
+
+internal class Arquivo
+{
+    public string Id { get; set; }
+    public bool Deletado { get; set; }
+    public bool Ativo { get; set; } = true;
+    public DateTime? DataCadastro { get; set; }
+    public DateTime? DataAtualizacao { get; set; }
+    public string CnpjEmpresa { get; private set; }
+    public string NomeEmpresa { get; private set; }
+    public string CpfCnpjCadastro { get; private set; }
+    public string NomeCadastro { get; private set; }
+    public int Serie { get; private set; }
+    public int NumeroNota { get; private set; }
+    public string ChaveAcesso { get; private set; }
+    public int Situacao { get; private set; }
+    public double TotalNota { get; private set; }
+    public DateTime DataEmissao { get; private set; }
+    public DateTime DataNota { get; private set; }
+    public DateTime? DataEntrada { get; private set; }
+    public DateTime DataPadrao { get; private set; }
+    public int Operacao { get; private set; }
+    public int TipoNota { get; private set; }
+    public int Status { get; private set; }
+    public string SpacesKey { get; private set; }
+    public string MotivoFalha { get; private set; }
+    public string UrlExterna { get; private set; }
+    public string SpacesKeyCancelamento { get; private set; }
+
+    public Arquivo(string id, string cnpjEmpresa, string nomeEmpresa, string cpfCnpjCadastro, string nomeCadastro, int serie, int numeroNota, string chaveAcesso, int situacao, double totalNota, DateTime dataEmissao, DateTime dataNota, DateTime? dataEntrada, int operacao, int tipoNota, int status, string urlExterna, DateTime? dataCadastro)
+    {
+        Id = id;
+        DataCadastro = DateTime.Now;
+        DataAtualizacao = DateTime.Now;
+        CnpjEmpresa = cnpjEmpresa;
+        NomeEmpresa = nomeEmpresa;
+        CpfCnpjCadastro = cpfCnpjCadastro;
+        NomeCadastro = nomeCadastro;
+        Serie = serie;
+        NumeroNota = numeroNota;
+        ChaveAcesso = chaveAcesso;
+        Situacao = situacao;
+        TotalNota = totalNota;
+        DataEmissao = dataEmissao;
+        DataNota = dataNota;
+        DataEntrada = dataEntrada;
+        TipoNota = tipoNota;
+        DataPadrao = DateTime.Now;
+        Operacao = operacao;
+        Status = status;
+        UrlExterna = urlExterna;
+        DataCadastro = dataCadastro;
+        SpacesKey = "";
+        SpacesKeyCancelamento = "";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using ClickHouse.Client.ADO;
+using ClickHouse.Client.Copy;
+using ClickHouse.Client.Utility;
+using FastMember;
+using IssueClickHouseClient;
+
+ClickHouseConnection clickHouseConnection = new($"Host=;Protocol=http;Port=;Username=;Password=;Compress=true;");
+
+await CriarTabelasPadraoAmigoContador();
+await InserirArquivosPorBulkCopy(ObterArquivos());
+
+async Task InserirArquivosPorBulkCopy(List<Arquivo> arquivos)
+{
+    if (!arquivos.Any()) return;
+
+    using ClickHouseBulkCopy bulkCopyInterface = new(clickHouseConnection)
+    {
+        DestinationTableName = "amigo_contador.arquivo",
+    };
+
+    using var reader = ObjectReader.Create(arquivos, "Id", "Deletado", "Ativo", "DataCadastro", "DataAtualizacao", "CnpjEmpresa", "NomeEmpresa", "CpfCnpjCadastro", "NomeCadastro", "Serie", "NumeroNota", "ChaveAcesso", "Situacao", "TotalNota", "DataEmissao", "DataNota", "DataEntrada", "DataPadrao", "Operacao", "TipoNota", "Status", "SpacesKey", "MotivoFalha", "UrlExterna", "SpacesKeyCancelamento");
+
+    while (reader.Read())
+        await bulkCopyInterface.WriteToServerAsync(reader);
+}
+
+async Task CriarTabelasPadraoAmigoContador()
+{
+    const string DATABASE_NAME = "amigo_contador";
+
+    await clickHouseConnection.ExecuteScalarAsync($"CREATE DATABASE IF NOT EXISTS {DATABASE_NAME}");
+
+    await clickHouseConnection.ExecuteScalarAsync($"CREATE TABLE IF NOT EXISTS {DATABASE_NAME}.arquivo ( Id String, Deletado Bool, Ativo Bool, DataCadastro DateTime, DataAtualizacao DateTime, CnpjEmpresa FixedString(14), NomeEmpresa String, CpfCnpjCadastro String, NomeCadastro String, Serie Int32, NumeroNota Int32, ChaveAcesso String, Situacao Int8, TotalNota Float32, DataEmissao DateTime, DataNota DateTime, DataEntrada Nullable(DateTime), DataPadrao DateTime, Operacao UInt8, TipoNota UInt8, Status UInt8, SpacesKey String, MotivoFalha String DEFAULT '', UrlExterna String, SpacesKeyCancelamento String DEFAULT '' ) ENGINE = ReplacingMergeTree(DataAtualizacao) PRIMARY KEY (ChaveAcesso, CnpjEmpresa) PARTITION BY CnpjEmpresa ORDER BY (ChaveAcesso, CnpjEmpresa) SETTINGS index_granularity = 8192");
+}
+
+List<Arquivo> ObterArquivos()
+{
+    List<Arquivo> arquivos = new()
+    {
+        new Arquivo("a1B2C4878A6", "123456789", "Teste", "123456789", "Teste", 123456789, 123465, "teste", 0, 10.2, DateTime.Now, DateTime.Now, DateTime.Now, 0, 0, 0, "Teste", DateTime.Now), //this value doesn't insert
+        new Arquivo("aa", "12345678", "Teste", "123456789", "Teste", 123456789, 123465, "teste", 0, 10.2, DateTime.Now, DateTime.Now, DateTime.Now, 0, 0, 0, "Teste", DateTime.Now),
+        new Arquivo("bb", "12345789", "Teste", "123456789", "Teste", 123456789, 123465, "teste", 0, 10.2, DateTime.Now, DateTime.Now, DateTime.Now, 0, 0, 0, "Teste", DateTime.Now),
+        new Arquivo("cc", "12356789", "Teste", "123456789", "Teste", 123456789, 123465, "teste", 0, 10.2, DateTime.Now, DateTime.Now, DateTime.Now, 0, 0, 0, "Teste", DateTime.Now),
+        new Arquivo("dd", "12456789", "Teste", "123456789", "Teste", 123456789, 123465, "teste", 0, 10.2, DateTime.Now, DateTime.Now, DateTime.Now, 0, 0, 0, "Teste", DateTime.Now)
+
+    };
+
+    return arquivos;
+}
